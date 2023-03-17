9d0c5792510d1e4629b4fc191ba94b0a649e0c57
VadimShicha <75353180+VadimShicha@users.noreply.github.com>
2023-03-16 23:11:32 -0700

    Background, nav bar background

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -16,40 +16,47 @@ function NavBar(props)
         
         if(index != -1)
             classNames[index][1](selectedClass);
+
+        console.log(index);
     }
 
-    useEffect(function()
+    useEffect(() =>
     {
-        //setSelectedIndex(props.page);
-        switch(props.page)
-        {
-            case "":
-                setSelectedIndex(0);
-                break;
-            case "multiplayer":
-                setSelectedIndex(1);
-                break;
-            case "social":
-                setSelectedIndex(2);
-                break;
-            case "settings":
-                setSelectedIndex(3);
-                break;
-            default:
-                setSelectedIndex(-1);
-                return;
-        }
-    }, [props.page]);
+        setSelectedIndex(props.page);
+    }, []);
+
+    // useEffect(function()
+    // {
+    //     //setSelectedIndex(props.page);
+    //     switch(props.page)
+    //     {
+    //         case "":
+    //             setSelectedIndex(0);
+    //             break;
+    //         case "multiplayer":
+    //             setSelectedIndex(1);
+    //             break;
+    //         case "social":
+    //             setSelectedIndex(2);
+    //             break;
+    //         case "settings":
+    //             setSelectedIndex(3);
+    //             break;
+    //         default:
+    //             setSelectedIndex(-1);
+    //             return;
+    //     }
+    // }, [props.page]);
 
     return (
         <div className="nav_bar">
             <ul className="nav_bar_ul">
                 <img className="nav_bar_image" src="./assets/ico.svg"></img>
                 <li className="nav_bar_title nav_bar_li"><b>Socket Games</b></li>
-                <li className={classNames[0][0]}><a href="/">Singleplayer</a></li>
-                <li className={classNames[1][0]}><a href="/multiplayer">Multiplayer</a></li>
-                <li className={classNames[2][0]}><a href="/social">Social</a></li>
-                <li className={classNames[3][0]}><a href="/settings">Settings</a></li>
+                <li className={`${classNames[0][0]} nav_bar_text`}><a href="/">Singleplayer</a></li>
+                <li className={`${classNames[1][0]} nav_bar_text`}><a href="/multiplayer">Multiplayer</a></li>
+                <li className={`${classNames[2][0]} nav_bar_text`}><a href="/social">Social</a></li>
+                <li className={`${classNames[3][0]} nav_bar_text`}><a href="/settings">Settings</a></li>
                 {/* <li style={{position: "fixed", right: "0px", padding: "6px", fontSize: "20px"}}>Username</li> */}
             </ul>
         </div>
